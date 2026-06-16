34163c814a05ee57707e31e890edee3e339683d7
legacyjackson <julius@globalinvestmentcompanies.com>
2026-06-16 13:47:46 -0700

    Update page.tsx

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,8 @@
-export const dynamic = 'force-dynamic'
-  
+
 'use client'
 
+
+export const dynamic = 'force-dynamic'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
